115e5f4eefc19dfb017411d0234d12666e41af2a
MacBook Pro <macbookpro@MacBook-Pro-de-MacBook.local>
2023-11-21 22:37:42 -0300

    Projections e Consultas SQL Nativas

--- src/main/java/com/devsperior/dslist/services/GameService.java
+++ src/main/java/com/devsperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsperior.dslist.dto.GameDTO;
 import com.devsperior.dslist.dto.GameMinDTO;
 import com.devsperior.dslist.entities.Game;
+import com.devsperior.dslist.projections.GameMinProjection;
 import com.devsperior.dslist.repositories.GameRepository;
 
 @Service
@@ -34,5 +35,14 @@ public class GameService {
 		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 		return dto;
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
+		
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
 	}
 }
